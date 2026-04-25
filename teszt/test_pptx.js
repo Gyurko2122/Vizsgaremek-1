4b6d4e1198c7a378aa3892b87070a911a8f30bd5
Gyurko2122 <szabomartin2007@gmail.com>
2026-04-25 10:26:52 +0200

    Done

--- teszt/test_pptx.js
+++
@@ -1,8 +0,0 @@
-﻿const PptxGenJS = require("pptxgenjs");
-const pres = new PptxGenJS();
-pres.layout = "LAYOUT_WIDE";
-pres.author = "Test";
-pres.title = "Test";
-const slide = pres.addSlide();
-slide.addText("Hello", { x: 1, y: 1, fontSize: 32, color: "000000" });
-pres.writeFile({ fileName: "test_pptx.pptx" }).then(()=>console.log("ok")).catch(e=>console.error(e));
